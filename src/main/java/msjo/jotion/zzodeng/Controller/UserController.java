2c7e395b3b84f956b952496df2560da637388a32
minsuJo <tok0419@naver.com>
2021-02-16 18:08:16 +0900

    front framework setting using angular

--- src/main/java/msjo/jotion/zzodeng/Controller/UserController.java
+++ src/main/java/msjo/jotion/zzodeng/Controller/UserController.java
@@ -6,6 +6,8 @@ import msjo.jotion.zzodeng.Repository.UserRepository;
 import msjo.jotion.zzodeng.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -28,23 +30,31 @@ public class UserController {
 
 
     @GetMapping("/api/users")
-    List<User> findAllUser() {
+    ResponseEntity<List<User>> findAllUser() {
 
-        return (List<User>) userRepository.findAll();
+        List<User> users = new ArrayList<User>();
+        Iterable<User> userIterable = userRepository.findAll();
+
+        userIterable.forEach(users::add);
+
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
 
     @GetMapping("/api/users/{userId}")
-    Optional<User> findUser(@PathVariable String userId) {
+    ResponseEntity<User> findUser(@PathVariable String userId) {
 
-        Optional<User> user = userRepository.findById(userId);
+        User user = userRepository.findById(userId).orElse(new User());
 
-        //null 일 때 처리 필요
+        //조회결과가 없을 때
+        if(user.get_id() == null)
+            return new ResponseEntity<User>(user, HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<User>(user, HttpStatus.OK);
 
-        return user;
     }
 
     @PostMapping(path = "/api/users")
-    String register(@RequestBody @Valid User user, Errors errors) {
+    ResponseEntity<String> register(@RequestBody @Valid User user, Errors errors) {
 
 
         //validation Error 처리
@@ -55,19 +65,19 @@ public class UserController {
                 buffer.append(error.getDefaultMessage() + "\n");
             }
 
-            return buffer.toString();
+            return new ResponseEntity<String>(buffer.toString(), HttpStatus.BAD_REQUEST);
         }
         else if(userRepository.findById(user.get_id()).isPresent())
-            return "Id already exist!";
+            return new ResponseEntity<String>("Id already exist!", HttpStatus.FOUND);
         else {
             userRepository.save(user);
-            return "register is success!";
+            return new ResponseEntity<String>("register is success!\nThanks " + user.getName(), HttpStatus.CREATED);
         }
 
     }
 
     @PutMapping(path = "/api/users/{userId}")
-    String editUserInfo(@PathVariable String userId, @RequestBody @Valid User user, Errors errors) {
+    ResponseEntity<String> editUserInfo(@PathVariable String userId, @RequestBody @Valid User user, Errors errors) {
 
         //validation Error 처리
         if(errors.hasErrors()) {
@@ -77,14 +87,23 @@ public class UserController {
                 buffer.append(error.getDefaultMessage() + "\n");
             }
 
-            return buffer.toString();
+            return new ResponseEntity<String>(buffer.toString(), HttpStatus.BAD_REQUEST);
         }
-        else if(!userRepository.findById(user.get_id()).orElse(new User()).equals(userId))
-            return "you cannot change ID!";
+        else if(!userRepository.findById(user.get_id()).orElse(new User())
+                                                       .get_id().equals(userId))
+            return new ResponseEntity<String>("you cannot change ID! + ", HttpStatus.BAD_REQUEST);
         else {
             userRepository.save(user);
-            return "your information editing is success!";
+            return new ResponseEntity<String>("your information editing is success!", HttpStatus.OK);
         }
+    }
+
+    @DeleteMapping(path = "/api/users/{userId}")
+    ResponseEntity<String> withdrawal(@PathVariable String userId) {
+
+        userRepository.deleteById(userId);
+
+        return new ResponseEntity<>("Thanks for using our sevice!", HttpStatus.NO_CONTENT);
     }
 
 
